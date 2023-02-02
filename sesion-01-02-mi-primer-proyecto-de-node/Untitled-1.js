09e706aa49b34941cfb98923765e965763bda4fb
ivangdg <63275713+ivangdg@users.noreply.github.com>
2023-02-01 20:25:03 -0600

    ejercicio6

---
+++ sesion-01-02-mi-primer-proyecto-de-node/Untitled-1.js
@@ -0,0 +1,10 @@
+
+if (3!==3)
+
+
+if (3 === "3") {
+    console.log("verdadero")
+}
+else {
+    console.log("falso")
+}
--- sesion-01-02-mi-primer-proyecto-de-node/hola_mundo.js
+++ sesion-01-02-mi-primer-proyecto-de-node/hola_mundo.js
@@ -1,1 +1,6 @@
 console.log("Hola Mundo");
+
+let valor = 5
+
+const nuevo = valor()
+
